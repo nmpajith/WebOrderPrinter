883810bb79a53f8ab5b63c7290b7db21a94e1d3f
girihell <girihell@yahoo.com>
2021-06-23 11:01:32 +0530

    DeviceDetails Added

---
+++ BusinessLogicLayer/Models/Devices/DeviceDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Models.Devices
+{
+    class DeviceDetails
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int RestId { get; set; }
+        public int BranchId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+    }
+}
