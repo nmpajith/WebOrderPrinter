fa750cc9d86fcdc584ca461973dc6deea37dba5a
girihell <girihell@yahoo.com>
2021-06-23 11:12:24 +0530

    Notifications.cs Added

---
+++ BusinessLogicLayer/Models/Notifications/Notifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Models.Notifications
+{
+    class Notifications
+    {
+        public int Id { get; set; }
+        public int NotificationTypeId { get; set; }
+        public int RestId { get; set; }
+        public int BranchId { get; set; }
+        public int DeviceId { get; set; }
+        public int OrderId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
