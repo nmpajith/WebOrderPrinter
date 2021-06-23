90038674acef0908b485cad62c10e2cebad1cebe
Ajith Kumara <nmpajith@gmail.com>
2021-06-23 12:22:36 +0530

    Added Order Model Classes

---
+++ BusinessLogicLayer/Models/Orders/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Models.Orders
+{
+    public class OrderStatus
+    {
+        public int Id { get; set; }
+        public int OrderStatusTypeId { get; set; }
+        public int OrderDetailId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public OrderStatusType OrderStatusType { get; set; }
+    }
+}
