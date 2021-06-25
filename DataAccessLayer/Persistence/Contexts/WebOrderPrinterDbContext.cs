921e118fb00c6a16230b7e09a7740502f055fbcc
Ajith Kumara <nmpajith@gmail.com>
2021-06-25 21:57:10 +0530

    Added Data Context

---
+++ DataAccessLayer/Persistence/Contexts/WebOrderPrinterDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogicLayer.Models.Restaurants;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Persistence.Contexts
+{
+    public class WebOrderPrinterDbContext : DbContext
+    {
+        public DbSet<Restaurant> Categories { get; set; }
+        public DbSet<Branch> Products { get; set; }
+
+        public WebOrderPrinterDbContext() : base() { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);           
+
+        }
+    }
+}
