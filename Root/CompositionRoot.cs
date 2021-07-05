121c9ab5bd347420d3eb0b10d28dd8a76f232d92
Ajith Kumara <nmpajith@gmail.com>
2021-07-05 10:08:25 +0530

    Added Cmposition Root class

---
+++ Root/CompositionRoot.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root
+{
+    public class CompositionRoot
+    {
+        public CompositionRoot() { }
+
+        public static void RegisterDependencies(IServiceCollection services)
+        {
+        }
+    }
+}
