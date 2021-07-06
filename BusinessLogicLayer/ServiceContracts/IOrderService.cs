951f8ce27844e66b57230e1c8fa5d97de991690b
girihell <girihell@yahoo.com>
2021-07-06 23:37:40 +0530

    Added OrderService

---
+++ BusinessLogicLayer/ServiceContracts/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Models.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.ServiceContracts
+{
+    interface IOrderService
+    {
+        IEnumerable<OrderDetail> GetAllOrders();
+    }
+}
---
+++ BusinessLogicLayer/ServiceImplementations/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccessLayer.Repositories;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.ServiceImplementations
+{
+    public class OrderService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderService(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Category> GetAllOrders()
+        {
+            return _unitOfWork.OrderDetails.Get();
+        }
+    }
+}
