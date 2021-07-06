00f1c0783e2b9c7a3247bfe896d9e0f812de3737
Ajith Kumara <nmpajith@gmail.com>
2021-07-06 11:29:07 +0530

    Added Unitofwork

---
+++ DataAccessLayer/Repositories/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Models.Device;
+using Infrastructure.Models.Notifications;
+using Infrastructure.Models.Order;
+using Infrastructure.Models.Restaurant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IRepository<DeviceDetail> DeviceDetails { get; }
+        IRepository<OrderDetail> OrderDetails { get; }
+        IRepository<Restaurant> Restaurants { get; }
+        IRepository<Branch> Branches { get; }
+        IRepository<Notification> Notifications { get; }
+        void Commit();
+    }
+}
---
+++ DataAccessLayer/Repositories/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Persistence.Contexts;
+using Infrastructure.Models.Device;
+using Infrastructure.Models.Notifications;
+using Infrastructure.Models.Order;
+using Infrastructure.Models.Restaurant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private WebOrderPrinterDbContext _dbContext;
+        private BaseRepository<DeviceDetail> _deviceDetails;
+        private BaseRepository<OrderDetail> _orderDetails;
+        private BaseRepository<Restaurant> _restaurants;
+        private BaseRepository<Branch> _branches;
+        private BaseRepository<Notification> _notifications;
+
+        public UnitOfWork(WebOrderPrinterDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IRepository<DeviceDetail> DeviceDetails => _deviceDetails ??
+            (_deviceDetails = new BaseRepository<DeviceDetail>(_dbContext));
+
+        public IRepository<OrderDetail> OrderDetails => _orderDetails ??
+            (_orderDetails = new BaseRepository<OrderDetail>(_dbContext));
+
+        public IRepository<Restaurant> Restaurants => _restaurants ??
+            (_restaurants = new BaseRepository<Restaurant>(_dbContext));
+
+        public IRepository<Branch> Branches => _branches ??
+            (_branches = new BaseRepository<Branch>(_dbContext));
+
+        public IRepository<Notification> Notifications => _notifications ??
+            (_notifications = new BaseRepository<Notification>(_dbContext));
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
